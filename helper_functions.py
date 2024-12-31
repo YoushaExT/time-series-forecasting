bf32f252e7f6abf8749babe54362e9cada47214e
Yousha Arshad <yousha.arshad@techwards.co>
2025-01-01 02:36:10 +0500

    extract helpers

--- helper_functions.py
+++ helper_functions.py
@@ -24,3 +24,37 @@ def evaluate_model(test, forecast):
     r2 = r2_score(test, forecast)
     mape = mean_absolute_percentage_error(test, forecast)
     return rmse, mae, r2, mape
+
+
+def plot(data, cols):
+    # plot the time series data
+    plt.figure(figsize=(15, 6))
+    plt.plot(data[cols[0]], data[cols[1]])
+    plt.title("TSF")
+    plt.xlabel(cols[0])
+    plt.ylabel(cols[1])
+    plt.show()
+
+
+def plot_model_results(
+    train, test, forecast, forecast_train, label, cols, differencing=0
+):
+    _train = train.copy()
+    _test = test.copy()
+    if differencing:
+        _train = _train.iloc[differencing:]
+        _test = _test.iloc[differencing:]
+
+    # Plot Exponential Smoothing results
+    plt.figure(figsize=(15, 6))
+    plt.plot(train[cols[0]], train[cols[1]], label="Train", color="blue")
+    plt.plot(test[cols[0]], test[cols[1]], label="Test", color="green")
+    plt.plot(_train[cols[0]], forecast_train, label="In-sample Forecast", color="red")
+    plt.plot(
+        _test[cols[0]],
+        forecast,
+        label=label,
+        color="orange",
+    )
+    plt.legend(loc="best")
+    plt.show()
